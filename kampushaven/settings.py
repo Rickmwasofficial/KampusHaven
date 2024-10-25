54a947327d9ee60ca47f3b62c19899a1573d0540
Rickmwasofficial <146796160+Rickmwasofficial@users.noreply.github.com>
2024-10-25 07:58:50 +0300

    Update settings.py

--- kampushaven/settings.py
+++ kampushaven/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1_m1o*!!%ud-66j#m74ru4g392xm%*4a!2v-5=x#zm9fde+r(g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['maverick-blogs.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'kampushaven.urls'
@@ -118,9 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    os.path.join(BASE_DIR, 'static'),
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
